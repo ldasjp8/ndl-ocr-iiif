c135ceb3861a6a1e84da66b0196dc60aecf03d0a
nakamura196 <na.kamura.1263@gmail.com>
2022-04-03 08:02:19 +0900

    feat: fix layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,89 +2,83 @@
   <div>
     <Header />
 
-    <v-container class="my-5">
+    <v-container class="my-10">
       <div class="text-center">
         {{ siteDesc }}
       </div>
-      <v-row class="mt-5">
-        <v-col col="12" md="3"></v-col>
-        <v-col col="12" md="6">
-          <v-text-field
-            v-model="id"
-            rounded
-            filled
-            :placeholder="`${'IDを入力してください。'} ${$t(
-              'example'
-            )}: 3437686`"
-            @click:append="search"
-            @keydown.enter="trigger"
-          ></v-text-field>
-          <v-btn
-            class="ma-2"
-            large
-            rounded
-            depressed
-            color="primary"
-            @click="search"
-          >
-            {{ $t('execution') }}</v-btn
-          >
-          <v-btn
-            class="ma-2"
-            large
-            rounded
-            depressed
-            color="success"
-            @click="id = 3437686"
-          >
-            {{ $t('example') }} 校異源氏物語</v-btn
-          >
+      <div class="mt-10 text-center">
+        <v-text-field
+          v-model="id"
+          rounded
+          filled
+          :placeholder="`${'IDを入力してください。'} ${$t('example')}: 3437686`"
+          @click:append="search"
+          @keydown.enter="trigger"
+        ></v-text-field>
+        <v-btn
+          class="ma-2"
+          large
+          rounded
+          depressed
+          color="primary"
+          @click="search"
+        >
+          {{ $t('execution') }}</v-btn
+        >
+        <v-btn
+          class="ma-2"
+          large
+          rounded
+          depressed
+          color="success"
+          @click="id = 3437686"
+        >
+          {{ $t('example') }} 校異源氏物語</v-btn
+        >
 
-          <div class="mt-10">
-            <template v-if="status === 'RUNNING'">
-              <v-progress-linear
-                indeterminate
-                color="primary"
-                class="mt-3"
-              ></v-progress-linear>
-            </template>
-            <template v-else-if="status === 'FAILED'">
-              {{ $t('failed') }}
-            </template>
-            <template v-else-if="status === 'SUCCEEDED'">
-              <v-row>
-                <v-col>
-                  <v-btn
-                    :href="`https://mirador.cultural.jp/?manifest=${output.manifest}`"
-                    target="_blank"
-                    class="info"
-                    block
-                    rounded
-                    depressed
-                    large
-                  >
-                    Mirador
-                  </v-btn>
-                </v-col>
-                <v-col>
-                  <v-btn
-                    :href="`http://codh.rois.ac.jp/software/iiif-curation-viewer/demo/?curation=${output.curation}`"
-                    target="_blank"
-                    class="info"
-                    block
-                    rounded
-                    depressed
-                    large
-                  >
-                    Curation Viewer
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </template>
-          </div>
-        </v-col>
-        <v-col col="12" md="3"></v-col>
-      </v-row>
+        <div class="mt-10">
+          <template v-if="status === 'RUNNING'">
+            <v-progress-linear
+              indeterminate
+              color="primary"
+              class="mt-3"
+            ></v-progress-linear>
+          </template>
+          <template v-else-if="status === 'FAILED'">
+            {{ $t('failed') }}
+          </template>
+          <template v-else-if="status === 'SUCCEEDED'">
+            <v-row>
+              <v-col>
+                <v-btn
+                  :href="`https://mirador.cultural.jp/?manifest=${output.manifest}`"
+                  target="_blank"
+                  class="info"
+                  block
+                  rounded
+                  depressed
+                  large
+                >
+                  Mirador
+                </v-btn>
+              </v-col>
+              <v-col>
+                <v-btn
+                  :href="`http://codh.rois.ac.jp/software/iiif-curation-viewer/demo/?curation=${output.curation}`"
+                  target="_blank"
+                  class="info"
+                  block
+                  rounded
+                  depressed
+                  large
+                >
+                  Curation Viewer
+                </v-btn>
+              </v-col>
+            </v-row>
+          </template>
+        </div>
+      </div>
     </v-container>
 
     <Footer />
